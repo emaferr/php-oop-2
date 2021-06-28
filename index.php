39e22dcf3da95af73dfc9d2c8fb275342ec5df25
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-28 15:14:41 +0200

    Structure the classes by managing inheritance

--- index.php
+++ index.php
@@ -1,4 +1,59 @@
-<?php?>
+<?php
+class Product
+{
+    protected $path;
+    protected $name;
+    protected $desc;
+    protected $price;
+    protected $discount;
+    public function __construct(string $path, string $name, string $desc, float $price, int $discount = null)
+    {
+        $this->path = $path;
+        $this->name = $name;
+        $this->desc = $desc;
+        $this->price = $price;
+        $this->discount = $discount;
+    }
+ 
+}
+
+class Notebook extends Product {
+    function __construct(string $path, string $name, string $desc, float $price, int $discount = null, int $diagonal) 
+    {
+        parent::__construct($path, $name, $desc, $price, $discount);
+        $this->diagonal = $diagonal;
+    }
+}
+
+class Tablet extends Notebook {
+    function __construct(string $path, string $name, string $desc, float $price, int $discount = null, int $diagonal, string $connect) 
+    {
+        parent::__construct($path, $name, $desc, $price, $discount, $diagonal);
+        $this->connect = $connect;
+    }
+}
+
+
+$notebook = [
+
+    new Notebook ('https://', 'Apple MacBook Air M1', '8GB Ram', 1439.50, null, 14),
+    new Notebook ('https://', 'Asus Rog Zephyrus', '16GB Ram', 1439.50, null, 16),
+    new Notebook ('https://', 'Lenovo IdeaPad', '12GB Ram', 980.00, null, 15),
+    new Notebook ('https://', 'Acer aspire v-nitro', '24GB Ram', 1016.32, null, 15),
+    
+];
+
+var_dump($notebook);
+
+$tablet = [
+    new Tablet ('https://', 'Apple Ipad Air', '4GB Ram', 439.50, null, 7, '4G LTE'),
+    new Tablet ('https://', 'Samsung Tab', '6GB Ram', 639.50, null, 7, '4G LTE'),
+    new Tablet ('https://', 'Mediacom Pad X', '4GB Ram', 239.50, null, 7, 'Wi-Fi'),
+];
+
+var_dump($tablet);
+
+?>
 
 <!DOCTYPE html>
 
