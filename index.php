2ad3cfd05d982e7579d1351719d587d5fe2151e8
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-28 13:30:50 +0200

    Install bootstrap

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php?>
 
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./dist/css/app.css">
 </head>
+
 <body>
-    <h1>Start</h1>
+
+    <h1 class="bg-dark text-white" >Start</h1>
 
     <script src="./dist/js/app.js"></script>
 </body>
+
 </html>
